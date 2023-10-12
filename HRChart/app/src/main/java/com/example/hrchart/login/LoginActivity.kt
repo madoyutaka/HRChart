cb3a9f831ebd38aec158f8071099398b921cc265
madoyutaka <shingesu19@gmail.com>
2023-10-12 18:02:28 +0900

    佐藤 ログイン画面作成（画面表示、画面遷移）

--- HRChart/app/src/main/java/com/example/hrchart/login/LoginActivity.kt
+++ HRChart/app/src/main/java/com/example/hrchart/login/LoginActivity.kt
@@ -3,17 +3,17 @@ package com.example.hrchart.login
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.hrchart.R
-import com.example.hrchart.login.view.LoginFragment
 
+/**
+ *  ログイン画面 Activity
+ *  画面ID:
+ *
+ *  @author Y.Sato
+ *  created on 2023/10/06
+ */
 class LoginActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, LoginFragment.newInstance())
-                .commitNow()
-        }
     }
 }
--- HRChart/app/src/main/java/com/example/hrchart/login/view/LoginViewModel.kt
+++ HRChart/app/src/main/java/com/example/hrchart/login/view/LoginViewModel.kt
@@ -1,7 +1,79 @@
 package com.example.hrchart.login.view
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
+/**
+ *  ログイン画面 ViewModel
+ *  画面ID:
+ *
+ *  @author Y.Sato
+ *  created on 2023/10/10
+ */
 class LoginViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    companion object {
+        /** TAG */
+        private const val TAG = "LoginViewModel"
+    }
+
+    //パスワード入力の検知
+    private val _isEnabled: MutableLiveData<Boolean> =
+        MutableLiveData<Boolean>().also { mutableLiveData ->
+            mutableLiveData.value = false
+        }
+    val isEnabled: LiveData<Boolean>
+        get() = _isEnabled
+
+    private val _buttonText: MutableLiveData<String> =
+        MutableLiveData<String>().also { mutableLiveData ->
+            mutableLiveData.value = "パスワードを入力してください。"
+        }
+    val buttonText: LiveData<String>
+        get() = _buttonText
+
+    //ボタン切り替え
+    fun updateButton(isBlank: Boolean) {
+        _isEnabled.value = !isBlank
+
+        if (!isBlank) {
+            _buttonText.value = "ログイン"
+        } else {
+            _buttonText.value = "パスワードを入力してください。"
+        }
+    }
+
+    //ログインボタン押下時
+    fun onLoginClicked(inputPassword: String): String {
+        Log.d(TAG, "onLoginClicked")
+
+        // テストデータ用のパスワード
+        val userPassword = "gwuser"
+        val adminPassword = "adminuser"
+
+        return passwordVerify(inputPassword)
+    }
+
+    //パスワード確認
+    private fun passwordVerify(inputPassword: String): String {
+        // テストデータ用のパスワード(DBから取得予定)
+        val userPassword = "gwuser"
+        val adminPassword = "adminuser"
+
+        return when (inputPassword) {
+            userPassword -> {
+                "user"
+            }
+
+            adminPassword -> {
+                "admin"
+            }
+
+            else -> {
+                "error"
+            }
+        }
+    }
 }
