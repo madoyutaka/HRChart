d4745007760882575e1f58c54d12ab4eae0dcdcd
madoyutaka <shingesu19@gmail.com>
2023-10-20 16:09:35 +0900

    佐藤 ログイン画面から従業員一覧画面への遷移処理 修正

--- HRChart/app/src/main/java/com/example/hrchart/login/LoginActivity.kt
+++ HRChart/app/src/main/java/com/example/hrchart/login/LoginActivity.kt
@@ -1,7 +1,15 @@
 package com.example.hrchart.login
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.drawable.ColorDrawable
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.MenuProvider
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.example.hrchart.R
 
 /**
@@ -12,8 +20,41 @@ import com.example.hrchart.R
  *  created on 2023/10/06
  */
 class LoginActivity : AppCompatActivity() {
+
+    // NavController初期化
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        navController = findNavController(R.id.nav_host_fragment)
+
+        // アクションバーの背景色
+        supportActionBar?.setBackgroundDrawable(ColorDrawable(getColor(R.color.light_gray)))
+
+        // ActionBarにNavigation機能を追加する
+        setupActionBarWithNavController(navController)
+
+        // ActionBarのメニュー制御
+        setupMenuBar()
+
+    }
+
+    /**
+     * setupMenuBar
+     * ActionBarのメニュー制御
+     */
+    private fun setupMenuBar() {
+        addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.custom_menu, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                // アイコンタップ時の処理はFragment側で制御するためfalseを返す
+                return false
+            }
+        })
     }
 }
--- HRChart/app/src/main/java/com/example/hrchart/login/view/LoginFragment.kt
+++ HRChart/app/src/main/java/com/example/hrchart/login/view/LoginFragment.kt
@@ -5,10 +5,16 @@ import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.core.widget.addTextChangedListener
 import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Lifecycle
 import androidx.navigation.fragment.NavHostFragment
 import com.example.hrchart.R
 import com.example.hrchart.databinding.FragmentLoginBinding
@@ -46,6 +52,8 @@ class LoginFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         Log.d(TAG, "onCreateView Start")
+        // アクションバー非表示
+        setupMenuBar()
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_login, container, false)
         viewModel = ViewModelProvider(this)[LoginViewModel::class.java]
         binding.viewModel = viewModel
@@ -56,19 +64,19 @@ class LoginFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        //パスワード入力のListener
+        // パスワード入力のListener
         binding.editTextPassword.addTextChangedListener { text ->
             viewModel.updateButton(text.isNullOrBlank())
         }
 
-        //ログインボタン押下のListener
+        // ログインボタン押下のListener
         binding.loginButton.setOnClickListener { v ->
-            //入力されたパスワード受け渡し
+            // 入力されたパスワード受け渡し
             inputPassword = binding.editTextPassword.text.toString()
-            //パスワード確認
+            // パスワード確認
             var loginType = viewModel.onLoginClicked(inputPassword)
 
-            //ログイン画面から従業員一覧画面へ画面遷移(仮)
+            // ログイン画面から従業員一覧画面へ画面遷移(仮)
             when (loginType) {
                 //
                 "user", "admin" -> {
@@ -78,14 +86,14 @@ class LoginFragment : Fragment() {
                     // NavController取得
                     val navController = navHostFragment.navController
                     val action =
-                        LoginFragmentDirections.actionLoginFragmentToBlankFragment(loginType = loginType)
+                        LoginFragmentDirections.actionLoginFragmentToNavGraphEmp()
                     navController.navigate(action)
                 }
-                //それ以外(パスワード不一致など)
+                // それ以外(パスワード不一致など)
                 else -> {
-                    //パスワード入力欄を空にして、エラーダイアログを表示
+                    // パスワード入力欄を空にして、エラーダイアログを表示
                     binding.editTextPassword.text = null
-                    //ダイアログ表示処理
+                    // ダイアログ表示処理
                     val dialog =  SimpleAlertDialogFragment("ログイン失敗", "パスワードが異なります。再入力してください。") {
                         // OK時の処理
                     }
@@ -93,6 +101,27 @@ class LoginFragment : Fragment() {
                 }
             }
         }
+    }
+
+    /**
+     * setupMenuBar
+     * ActionBarのメニュー制御
+     */
+    private fun setupMenuBar() {
+        val menuHost: MenuHost = requireActivity()
+        menuHost.addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+            }
+
+            override fun onPrepareMenu(menu: Menu) {
+                // ログアウトボタン非表示
+                menu.findItem(R.id.logout_menu_item).isVisible = false
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                return true
+            }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
     }
 
 }
